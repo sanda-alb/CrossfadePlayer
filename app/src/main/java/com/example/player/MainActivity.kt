5220919e64866de6285292034dae769978b7eb21
albrecht <sanda.albrecht@gmail.com>
2021-07-25 17:59:12 +0300

    load tracks

--- app/src/main/java/com/example/player/MainActivity.kt
+++ app/src/main/java/com/example/player/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.player
 
+import android.app.Activity
+import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.SeekBar
-import android.widget.TextView
+import android.provider.MediaStore
+import android.widget.*
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.player.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -21,8 +24,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var addFirstTrack: Button
     lateinit var addSecondTrack: Button
 
-    private val firstTrack: Uri = Uri.EMPTY
-    private val secondTrack: Uri = Uri.EMPTY
+    private var firstTrack: Uri = Uri.EMPTY
+    private var secondTrack: Uri = Uri.EMPTY
     val mediaPlayers = mutableListOf<MediaPlayer>(MediaPlayer(), MediaPlayer())
 
     lateinit var crossfadeBar: SeekBar
@@ -44,5 +47,47 @@ class MainActivity : AppCompatActivity() {
         crossfadeText = binding.crossfadeTime
         crossfadeText.text = crossfadeTime.toString()
         playButton = binding.playButton
+        Toast.makeText(this, "Add two tracks and set crossfade time", Toast.LENGTH_LONG).show()
+
+        selectTrack()
+    }
+
+
+    fun selectTrack() {
+        addFirstTrack.setOnClickListener{
+            addTrack(resultLauncher1)
+        }
+        addSecondTrack.setOnClickListener {
+            addTrack(resultLauncher2)
+        }
+    }
+
+    fun addTrack(resultLauncher: ActivityResultLauncher<Intent>) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "audio/*"
+        resultLauncher.launch(intent)
+    }
+
+    val resultLauncher1 = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    {result ->
+        loadTrack(mediaPlayers.first(), result)
+        firstTrack = result.data?.data!!
+    }
+
+    val resultLauncher2 = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    {   result ->
+        loadTrack(mediaPlayers.last(), result)
+        secondTrack = result.data?.data!!
+    }
+
+    fun loadTrack(mediaPlayer: MediaPlayer, result: ActivityResult) {
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            if (data != null) {
+                mediaPlayer.reset()
+                mediaPlayer.setDataSource(this, data.data!!)
+                mediaPlayer.prepare()
+            }
+        }
     }
 }
